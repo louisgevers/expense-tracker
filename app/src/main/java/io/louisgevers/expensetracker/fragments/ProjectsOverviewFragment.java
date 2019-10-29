b2b2df3acd92725f98c3b63466e237c67bb5f61e
Louis Gevers <l.gevers@student.tudelft.nl>
2019-10-29 16:35:36 +0800

    Create example transition

--- app/src/main/java/io/louisgevers/expensetracker/fragments/ProjectsOverviewFragment.java
+++ app/src/main/java/io/louisgevers/expensetracker/fragments/ProjectsOverviewFragment.java
@@ -3,11 +3,16 @@ package io.louisgevers.expensetracker.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import io.louisgevers.expensetracker.R;
 
@@ -29,4 +34,12 @@ public class ProjectsOverviewFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_projects_overview, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button exampleButton = getActivity().findViewById(R.id.example_button);
+        exampleButton.setOnClickListener((v) -> {
+            Navigation.findNavController(v).navigate(R.id.action_projectsOverviewFragment_to_projectFragment);
+        });
+    }
 }
