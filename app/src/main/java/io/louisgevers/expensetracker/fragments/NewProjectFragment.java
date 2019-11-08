d898cff6a48d165e69ef04918f1f2f8aed551b8b
Louis Gevers <l.gevers@student.tudelft.nl>
2019-11-08 16:00:15 +0800

    Add extra navigation to NewProjectFragment

--- app/src/main/java/io/louisgevers/expensetracker/fragments/NewProjectFragment.java
+++ app/src/main/java/io/louisgevers/expensetracker/fragments/NewProjectFragment.java
@@ -1,13 +1,19 @@
 package io.louisgevers.expensetracker.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import io.louisgevers.expensetracker.R;
 
@@ -29,4 +35,20 @@ public class NewProjectFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_new_project, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Toolbar toolbar = view.findViewById(R.id.newProjectToolbar);
+        toolbar.setNavigationOnClickListener(v ->
+                Navigation.findNavController(view).navigateUp());
+        view.findViewById(R.id.saveButton).setOnClickListener(v -> {
+                    Toast.makeText(getContext(), "Prototype: saving new projects not available yet", Toast.LENGTH_SHORT).show();
+                });
+
+        view.findViewById(R.id.chooseProjectImage).setOnClickListener(v -> {
+            Intent pickImageIntent = new Intent(
+                    Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(pickImageIntent, 108);
+        });
+    }
 }
