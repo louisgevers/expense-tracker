8faca89e5a253641aec552c26ca5f9b9f6a94a71
tsngaa <36004576+tsngaa@users.noreply.github.com>
2019-11-10 16:56:14 +0800

    Transition added for Project Page and Search Tab

--- app/src/main/java/io/louisgevers/expensetracker/fragments/SearchFragment.java
+++ app/src/main/java/io/louisgevers/expensetracker/fragments/SearchFragment.java
@@ -3,7 +3,10 @@ package io.louisgevers.expensetracker.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,4 +32,12 @@ public class SearchFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.listItem1).setOnClickListener(
+                v -> Navigation.findNavController(view).navigate(R.id.action_searchFragment_to_projectFragment));
+        view.findViewById(R.id.listItem2).setOnClickListener(
+                v -> Navigation.findNavController(view).navigate(R.id.action_searchFragment_to_detailsFragment));
+    }
 }
