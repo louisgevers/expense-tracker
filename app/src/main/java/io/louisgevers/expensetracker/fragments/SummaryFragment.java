0267eeddbd6fecd7a92b6b66dfcf83c38ced4a3c
csKangGT <gceohoony@gmail.com>
2019-11-10 17:47:17 +0800

    updating for the summary screen

--- app/src/main/java/io/louisgevers/expensetracker/fragments/SummaryFragment.java
+++ app/src/main/java/io/louisgevers/expensetracker/fragments/SummaryFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -35,8 +36,13 @@ public class SummaryFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Toolbar toolbar = view.findViewById(R.id.newProjectToolbar2);
+        toolbar.setNavigationOnClickListener(v ->
+                Navigation.findNavController(view).navigateUp());
+
         view.findViewById(R.id.paymentButton).setOnClickListener(
                 v -> Navigation.findNavController(v).navigate(R.id.action_summaryFragment_to_paymentFragment)
         );
+
     }
 }
