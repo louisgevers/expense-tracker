4a98206d858ab12f7db92f3fd513bcb8b9e7d86c
Eva <ychen840@gatech.edu>
2019-11-10 16:50:51 +0800

    Add back button transitions

--- app/src/main/java/io/louisgevers/expensetracker/fragments/DetailsFragment.java
+++ app/src/main/java/io/louisgevers/expensetracker/fragments/DetailsFragment.java
@@ -3,12 +3,16 @@ package io.louisgevers.expensetracker.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.navigation.Navigation;
 import io.louisgevers.expensetracker.R;
 
 /**
@@ -27,6 +31,14 @@ public class DetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Toolbar toolbar = view.findViewById(R.id.deatilToolbar);
+        toolbar.setNavigationOnClickListener(v ->
+                Navigation.findNavController(view).navigateUp());
     }
 
 }
